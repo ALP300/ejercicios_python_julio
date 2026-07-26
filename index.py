19745173610225adf74c27b555417bb914d5e0a7
ALP300 <aitorpalomino1@gmail.com>
2026-07-25 21:02:23 -0500

    a

--- index.py
+++ index.py
@@ -1,1 +1,18 @@
-print("Hola")
+numero= int(input("Ingrese un numero: "))
+numeros1= int(input("Ingrese otro numero: "))
+suma= numeros1+numero
+resta= numeros1-numero
+mult= numeros1*numero
+divi= numeros1/numero
+residuo= numeros1%numero
+potencia= numeros1**numero
+
+print(f"la suma de {numero} y {numeros1} es {suma}")
+print(f"la resta de {numero} y {numeros1} es {resta}")
+print(f"la multiplicacion de {numero} y {numeros1} es {mult}")
+print(f"la division de {numero} y {numeros1} es {divi}")
+print(f"el residuo de {numero} y {numeros1} es {residuo}")
+
+
+
+
